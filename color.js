d7bd38c864af601578bcd625a27056ca8e8b3c71
asp <asp@aptech.com>
2026-09-25 10:08:10 +0500

    first commit

---
+++ color.js
@@ -0,0 +1,8 @@
+const chalk = require("chalk")
+ 
+console.log(chalk.green("Good Morning!!"));
+console.log(chalk.blue("Ladiesw and gentlemen!"));
+console.log(chalk.yellow("Hello World!!"));
+console.log(chalk.red("Aptech Learning!!"));
+
+console.log(chalk.yellow.underline("Aptech Learning!!"));
---
+++ global.js
@@ -0,0 +1,11 @@
+console.log("Hello, This is Backend");
+
+console.log("Message:1 This Print First.");
+
+setTimeout(()=>{
+    console.log("Message:3 This Print After 5 Second Delay")
+}, 500)
+
+console.log("Message:2 This Print Second, right away");
+
+console.log("Message:4 ")
